a36be19d345795b6211757611ab390fd21a2617a
sophia-estrela <sophia.m.p.estrela@gmail.com>
2019-09-07 16:07:53 -0300

    Contact List working

--- eggshell/src/ContactList.js
+++ eggshell/src/ContactList.js
@@ -19,76 +19,70 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+function Contact({i},{props})
+{
+    const classes = useStyles();
+
+    return(
+    <ListItem 
+    alignItems="flex-start" 
+    onClick={()=> props.onClick}>
+        <ListItemAvatar>
+            <Avatar alt= {props.groups[i].name} src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+            primary = {props.groups[i].name}
+            secondary ={
+                <React.Fragment>
+                    <Typography
+                        component="span"
+                        variant="body2"
+                        className={classes.inline}
+                        color="textPrimary"
+                    >
+                        {props.groups[i].last_name}
+                    </Typography>
+                    {" — " + props.groups[i].last_message}
+                </React.Fragment>
+            }
+        />
+    </ListItem>
+    );
+}
+
 export default function ContactList() {
-  const classes = useStyles();
+  
+    const classes = useStyles();
+    const props = {
+        groups : [
+            {
+                name: 'Animes unidos',
+                last_name: 'Carlos',
+                last_message : 'Eu tentei!'
+            },
+            {
+                name: 'LGBT',
+                last_name: 'Luiz Anonimo',
+                last_message : 'Vou morrer!'
+            }]
+    }
+  const itens=[];
+  console.log(props.groups.length);
+  for (let i = 0; i<props.groups.length;i++)
+  {
+    itens.push(Contact({i},{props}));
+  }
 
   return (
-    <List className={classes.root}>
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Brunch this weekend?"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                Ali Connors
-              </Typography>
-              {" — I'll be in your neighborhood doing errands this…"}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Summer BBQ"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                to Scott, Alex, Jennifer
-              </Typography>
-              {" — Wish I could come, but I'm out of town this…"}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Oui Oui"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                Sandra Adams
-              </Typography>
-              {' — Do you have Paris recommendations? Have you ever…'}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-    </List>
+
+    <div>
+           <List className={classes.root}>
+            {
+               itens
+            }
+            </List> 
+        
+    </div>
+
   );
 }
