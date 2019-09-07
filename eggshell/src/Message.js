36bd26a9be2a7a13d6e95ab365822e7c4670e650
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-09-07 15:15:29 -0300

    Create message component

---
+++ eggshell/src/Message.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  inline: {
+    display: 'inline',
+  },
+}));
+
+export default function Message({name, self, message}) {
+  const classes = useStyles();
+
+  return (
+    <ListItem alignItems="flex-start">
+      {!self ? (
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+      ):
+      (<div></div>)
+      }
+      <ListItemText
+        primary={name}
+        secondary={
+          <React.Fragment>
+            <Typography
+              component="span"
+              variant="body2"
+              className={classes.inline}
+              color="textPrimary"
+            >
+              {/* Ali Connors */}
+              {message}
+            </Typography>
+            {/* {" — I'll be in your neighborhood doing errands this…"} */}
+          </React.Fragment>
+        }
+      />
+      {self ? (
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+      ):
+      (<div></div>)
+      }
+    </ListItem>
+  );
+}
