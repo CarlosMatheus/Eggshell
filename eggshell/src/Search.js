6b4484be449c37bf467e56fa8ba56dd5edacbcc1
Gabriel Kanegae <gabrielksouza2009@hotmail.com>
2019-09-07 16:13:12 -0300

    add avatars from json

--- eggshell/src/Search.js
+++ eggshell/src/Search.js
@@ -8,9 +8,6 @@ import Divider from '@material-ui/core/Divider';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
-import photo1 from './assets/1.png';
-import photo2 from './assets/2.png';
-import photo3 from './assets/3.png';
 import GroupJoin from './GroupJoin';
 import groupJson from './groups.json';
 import { StylesProvider } from '@material-ui/styles';
@@ -56,9 +53,9 @@ export default function PrimarySearchAppBar() {
   const [join, setJoin] = useState(0);
   const [info, setInfo] = useState(0);
 
-  const handleClick = (id, groupName, groupDescription) => {
+  const handleClick = (id, groupName, groupDescription, groupAvatar) => {
     setJoin(true);
-    setInfo({"id": id, "groupName": groupName, "groupDescription": groupDescription});
+    setInfo({"id": id, "groupName": groupName, "groupDescription": groupDescription, "groupAvatar": groupAvatar});
   }
 
   if (join) {
@@ -66,22 +63,19 @@ export default function PrimarySearchAppBar() {
   }
 
   let rows = []
-  let photos = [photo1, photo2, photo3];
-
-  console.log(groupJson);
 
   for (let i = 0; i < 3; i++) {
-
-    if (groupJson.groups[i].memberList.includes(1)) {
+    if (!groupJson.groups[i].memberList.includes(1)) {
         let name = groupJson.groups[i].displayName;
         let description = groupJson.groups[i].description;
+        let avatar = groupJson.groups[i].displayPicture;
 
         rows.push(
             <React.Fragment>
-            <ListItem alignItems="flex-start" onClick={(itemid, groupName, groupDescription) => 
-                handleClick(i, name, description )}>
+            <ListItem alignItems="flex-start" onClick={(itemid, groupName, groupDescription, groupAvatar) => 
+                handleClick(i, name, description, avatar)}>
                 <ListItemAvatar>
-                <Avatar alt="Av" src={photos[i]} />
+                <Avatar alt="AA" src={avatar}/>
                 </ListItemAvatar>
                 <ListItemText
                 primary={name}
@@ -108,37 +102,10 @@ export default function PrimarySearchAppBar() {
               inputProps={{ 'aria-label': 'search' }}
             />
         </div>
+        
         <List className={classes.root}>
             {rows}
-
-            {/* <ListItem alignItems="flex-start" onClick={(itemid, groupName, groupDescription) => handleClick("1", "Grupo 1", "Descricao grupo 1")}>
-                <ListItemAvatar>
-                <Avatar alt="Av" src={photo1} />
-                </ListItemAvatar>
-                <ListItemText
-                primary="Grupo 1"
-                />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-
-            <ListItem alignItems="flex-start" onClick={(itemid, groupName, groupDescription) => handleClick("2", "Grupo 2", "Descricao grupo 2")}>
-                <ListItemAvatar>
-                <Avatar alt="Av" src={photo2} />
-                </ListItemAvatar>
-                <ListItemText
-                primary="Grupo 2"
-                />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem alignItems="flex-start" onClick={(itemid, groupName, groupDescription) => handleClick("3", "Grupo 3", "Descricao grupo 3")}>
-                <ListItemAvatar>
-                <Avatar alt="Av" src={photo3} />
-                </ListItemAvatar>
-                <ListItemText
-                primary="Grupo 3"
-                />
-            </ListItem> */}
-            </List>
+        </List>
       </React.Fragment>
   );
 }
